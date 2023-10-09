d0d8239d95d3ddecf02f88abc4a47de572831fad
Lasse Duuni <lasse.raty@digitalworkforce.com>
2023-10-09 14:35:38 +0300

    Save receipts.zip in output folder

--- tasks.py
+++ tasks.py
@@ -7,6 +7,7 @@ from RPA.PDF import PDF
 from RPA.FileSystem import FileSystem
 from RPA.Archive import Archive
 
+
 @task
 def order_robots_from_RobotSpareBin():
     """
@@ -25,9 +26,11 @@ def order_robots_from_RobotSpareBin():
         get_robot_preview()
         order_number = submit_order()
         store_receipt_as_pdf(order_number)
-        embed_screenshot_to_receipt('preview.png', "output/receipts/" + order_number + "_receipt.pdf")
+        embed_screenshot_to_receipt(
+            "preview.png", "output/receipts/" + order_number + "_receipt.pdf"
+        )
     archive_receipts()
-    remove_receipts_directory()
+    remove_temp_files()
 
 
 def open_robot_order_website():
@@ -39,11 +42,11 @@ def get_orders():
     """Downloads the orders csv and returns it in a Table"""
     http = HTTP()
     http.download(url="https://robotsparebinindustries.com/orders.csv", overwrite=True)
-    
+
     library = Tables()
     orders = library.read_table_from_csv(
         "orders.csv", columns=["Order number", "Head", "Body", "Legs", "Address"]
-        )
+    )
     return orders
 
 
@@ -58,18 +61,20 @@ def fill_the_form(order):
     page = browser.page()
     page.select_option("#head", str(order["Head"]))
     page.check("#id-body-" + str(order["Body"]))
-    page.fill('input[placeholder="Enter the part number for the legs"]', str(order["Legs"]))
+    page.fill(
+        'input[placeholder="Enter the part number for the legs"]', str(order["Legs"])
+    )
     page.fill("#address", order["Address"])
-    
+
 
 def get_robot_preview():
     """Gets a screenshot of the robot preview"""
     page = browser.page()
     page.click("#preview")
     for i in range(1, 4):
-        page.wait_for_selector(f'#robot-preview-image > :nth-child({i})')
-    element = page.locator('#robot-preview-image')
-    element.screenshot(path='preview.png')
+        page.wait_for_selector(f"#robot-preview-image > :nth-child({i})")
+    element = page.locator("#robot-preview-image")
+    element.screenshot(path="preview.png")
 
 
 def submit_order():
@@ -79,7 +84,9 @@ def submit_order():
     for _ in range(5):  # Retry up to 5 times
         try:
             page.click("#order")
-            page.wait_for_selector("#order-completion", timeout=2000) # Tiny wait stage just for sake of testing. TODO: Wait for error message at the same time
+            page.wait_for_selector(
+                "#order-completion", timeout=2000
+            )  # Tiny wait stage just for sake of testing. TODO: Wait for error message at the same time
             return page.query_selector(".badge.badge-success").inner_text()
         except Exception:
             print("Order completion element did not appear, retrying...")
@@ -97,19 +104,20 @@ def store_receipt_as_pdf(order_number):
 
 
 def embed_screenshot_to_receipt(screenshot, pdf_file):
-    """Embed screenshot of the robot to its receipt."""  
+    """Embed screenshot of the robot to its receipt."""
     pdf = PDF()
-    file=[screenshot]
+    file = [screenshot]
     pdf.add_files_to_pdf(files=file, target_document=pdf_file, append=True)
 
 
 def archive_receipts():
     """Creates a zip file of all the created receipts"""
     lib = Archive()
-    lib.archive_folder_with_zip('./output/receipts', 'receipts.zip')
+    lib.archive_folder_with_zip("./output/receipts", "./output/receipts.zip")
 
 
-def remove_receipts_directory():
-    """Removes the receipts directory as the receipts are already contained in a zip file"""
+def remove_temp_files():
+    """Removes the receipts directory, orders.csv and preview.png when these files are no longer needed"""
     lib = FileSystem()
-    lib.remove_directory("./output/receipts",recursive=True)
+    lib.remove_directory("./output/receipts", recursive=True)
+    lib.remove_files("orders.csv", "preview.png")
